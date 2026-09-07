2191bbcc8707cc6d60a5e5e58f5c443d68fc1ae2
HackMeoww <lemanhcs2006@gmail.com>
2026-09-08 00:30:46 +0700

    Initial project setup with User model

---
+++ TravelTour.API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelTour.API.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string PassWord { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Role { get; set; } = "Customer";
+
+    }
+}
