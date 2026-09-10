97fe26e0ab1283385c4b0640515c32ba3c23d241
HackMeoww <lemanhcs2006@gmail.com>
2026-09-10 23:12:51 +0700

    Add Tour model

---
+++ TravelTour.API/Models/Tour.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelTour.API.Models
+{
+    public class Tour
+    {
+        public int TourId { get; set; }
+        public string TourName { get; set; } = string.Empty;
+        public string Destination { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Duration { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
