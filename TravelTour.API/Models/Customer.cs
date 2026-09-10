bdfedab619b854037966f1ab868daaaf421afc1a
HackMeoww <lemanhcs2006@gmail.com>
2026-09-10 23:02:29 +0700

    Add Customer model

---
+++ TravelTour.API/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelTour.API.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public int UserId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
