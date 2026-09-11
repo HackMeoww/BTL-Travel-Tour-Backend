a1e94248f16cb7f6f9617360e42aae0bdfc23069
HackMeoww <lemanhcs2006@gmail.com>
2026-09-11 11:05:59 +0700

    Add Booking model

---
+++ TravelTour.API/Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelTour.API.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public int TourId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfPeople { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Status { get; set; } = "Pending";
+    }
+}
