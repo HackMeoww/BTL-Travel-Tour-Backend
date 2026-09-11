e95ecb99d310e6bc7bc34e89001997dc92dfcf21
HackMeoww <lemanhcs2006@gmail.com>
2026-09-11 11:27:42 +0700

    Add Review model

---
+++ TravelTour.API/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelTour.API.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public int TourId { get; set; }
+        public int CustomerId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
