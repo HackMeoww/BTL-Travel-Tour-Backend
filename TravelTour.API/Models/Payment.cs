c9cdbf263a8c854d0e41c651da826789c61d0a13
HackMeoww <lemanhcs2006@gmail.com>
2026-09-11 11:23:14 +0700

    Add Payment model

---
+++ TravelTour.API/Models/Payment.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelTour.API.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int BookingId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string Status { get; set; } = "Paid";
+    }
+}
