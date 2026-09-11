c465da980d04fa771073950c3081f8d5c171bf56
HackMeoww <lemanhcs2006@gmail.com>
2026-09-11 09:53:47 +0700

    Add Itinerary model

---
+++ TravelTour.API/Models/Itinerary.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelTour.API.Models
+{
+    public class Itinerary
+    {
+        public int ItineraryId { get; set; }
+        public int TourId { get; set; }
+        public int DayNumber { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
