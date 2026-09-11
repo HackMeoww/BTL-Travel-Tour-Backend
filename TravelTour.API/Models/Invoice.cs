cc229be1ad505cb6ab5ae090b2b24984627f6483
HackMeoww <lemanhcs2006@gmail.com>
2026-09-11 11:18:28 +0700

    Add Invoice model

---
+++ TravelTour.API/Models/Invoice.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelTour.API.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int BookingId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentStatus { get; set; } = "Pending";
+    }
+}
