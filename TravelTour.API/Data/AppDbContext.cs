43f4db255ce132cdec9fa75d1483d2721d622014
HackMeoww <lemanhcs2006@gmail.com>
2026-09-13 21:55:47 +0700

    Add AppDbContext for database models

---
+++ TravelTour.API/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelTour.API.Models;
+
+namespace TravelTour.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+       public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<Itinerary> Itineraries { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
